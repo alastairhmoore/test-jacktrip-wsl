f4371d3bbaaf502627872a636ed6bf84933439ae
alastair <alastair@squaresetsound.com>
2020-09-30 09:40:44 +0100

    Added manual check of port connections on wsl side

--- test_tascar.ps1
+++ test_tascar.ps1
@@ -3,8 +3,8 @@ function Get-WSL2IPAdrress {
 }
 
 $client_ip=Get-WSL2IPAdrress
-"client:"
-$client_ip
+"client: " + $client_ip
+""
 
 # start the metronome process in new window
 Start-Process -FilePath "wsl" `
@@ -14,13 +14,25 @@ Start-Process -FilePath "wsl" `
 # show the list of ports (useful for debugging)
 # connect up the metronome to jacktrip
 #wsl -u root bash -c "sleep 0.1; jack_lsp; jack_connect metro:100_bpm JackTrip:send_1"
+wsl -u root bash -c "sleep 0.2; jack_lsp -c"
+
+""
+"The output above shows a list of connections and should include:"
+"render.main:out_l"
+"    JackTrip:send_1"
+"render.main:out_r"
+"    JackTrip:send_2"
+$dummy = Read-Host -Prompt 'Press return to continue'
+
 
 # connect jactrip to local soundcard
 & 'C:/Program Files (x86)/Jack/jack_connect.exe' ($client_ip + ':receive_1') system:playback_1
 & 'C:/Program Files (x86)/Jack/jack_connect.exe' ($client_ip + ':receive_2') system:playback_2
 
 # user check
-$dummy = Read-Host -Prompt 'Verify that you can hear the audio.../nPress return to end'
+""
+"Verify that you can hear the audio..."
+$dummy = Read-Host -Prompt "Press return to end"
 
 # disconnect and end the metronome process
 & 'C:/Program Files (x86)/Jack/jack_disconnect.exe' ($client_ip + ':receive_1') system:playback_1
